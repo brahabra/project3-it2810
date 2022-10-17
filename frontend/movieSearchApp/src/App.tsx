19afecf788bacdc71d12f6dc065b45c8a4752a9c
Brage Hagemann Brataas <bragehb@stud.ntnu.no>
2022-10-17 14:49:17 +0200

    #3 Updated branch with new code structure from main.

--- frontend/movieSearchApp/src/App.tsx
+++ frontend/movieSearchApp/src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './assets/svg/logo.svg';
-import './style/App.css';
+import "./style/App.css";
+import { useQuery, gql } from "@apollo/client";
 
 function App() {
+  const GET_MOVIES = gql`
+    query getMovies {
+      movies {
+        Poster_Link
+        Series_Title
+        IMDB_Rating
+      }
+    }
+  `;
+
+  function DisplayLocations() {
+    const { loading, error, data } = useQuery(GET_MOVIES);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+    return data.movies.map(
+      ({
+        Poster_Link,
+        Series_Title,
+        IMDB_Rating,
+      }: {
+        Poster_Link: string;
+        Series_Title: string;
+        IMDB_Rating: string;
+      }) => (
+        <div>
+          <h3>{Series_Title}</h3>
+          {/* <img width="450" height="450" alt="location-reference" src={`${Poster_Link.substring(0, 116)}`} /> */}
+          <img
+            width="50"
+            height="50"
+            alt="location-reference"
+            src={`${Poster_Link}`}
+          />
+          <br />
+          <p>IMDB Rating: {IMDB_Rating}</p>
+          <br />
+        </div>
+      )
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Movies</h3>
+      <DisplayLocations />
     </div>
   );
 }
