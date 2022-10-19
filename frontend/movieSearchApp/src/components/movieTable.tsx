e269e59d0d5c2b34c170cc45d4b8f74abc5ad3ba
Tor Madsen <tormads@ntnu.no>
2022-10-19 12:21:16 +0200

    #5 added event handlers for buttons

--- frontend/movieSearchApp/src/components/movieTable.tsx
+++ frontend/movieSearchApp/src/components/movieTable.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { IMovie, IMovies } from "../interfaces/IMovie";
 import "../style/movieTable.css";
 
@@ -25,37 +26,65 @@ export /**
  *
  * @param {IMovies} moviesProp
  */
-const MovieTableComp = (moviesProp: IMovies) => (
-  <div className="movieTableContainer">
-    {/* Table filtering */}
-    <div className="FilterBar">
-      <label>Genre</label>
-      <input type="text" />
-      <label>Actor</label>
-      <input type="text" />
-    </div>
-    {/* The table */}
-    <div className="movieTable">
-      <div className="movieTableDiscription">
-        <p>Movie Title</p>
-        <p>Movie Year</p>
-        <p>IMDB Rating</p>
+const MovieTableComp = (moviesProp: IMovies): JSX.Element => {
+  const [pagenum, setPagenum] = useState(0);
+
+  const buttonRightHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    //run switch page right function
+    alert("going right");
+  };
+  const buttonLeftHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    //run switch page left function
+    alert("going left");
+  };
+  // might use later:
+  // const inputPagenumHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   const enteredNumber: number = event.target.valueAsNumber;
+  //   if (!isNaN(enteredNumber)) {
+  //     setPagenum(enteredNumber);
+  //     alert("going to page " + enteredNumber);
+  //   }
+  //   //set pagunation number
+  // };
+
+  return (
+    <div className="movieTableContainer">
+      {/* Table filtering */}
+      <div className="FilterBar">
+        <label>Genre</label>
+        <input type="text" />
+        <label>Actor</label>
+        <input type="text" />
       </div>
-      {moviesProp.movieList.map((movie: IMovie) => {
-        return (
-          <MovieRowComp
-            Series_Title={movie.Series_Title}
-            Released_Year={movie.Released_Year}
-            IMDB_Rating={movie.IMDB_Rating}
-          />
-        );
-      })}
+      {/* The table */}
+      <div className="movieTable">
+        <div className="movieTableDiscription">
+          <p>Movie Title</p>
+          <p>Movie Year</p>
+          <p>IMDB Rating</p>
+        </div>
+        {moviesProp.movieList.map((movie: IMovie) => {
+          return (
+            <MovieRowComp
+              Series_Title={movie.Series_Title}
+              Released_Year={movie.Released_Year}
+              IMDB_Rating={movie.IMDB_Rating}
+            />
+          );
+        })}
+      </div>
+      {/* Page navigation */}
+      <div className="pageNavigation">
+        <button onClick={buttonLeftHandler}>&larr; prev page</button>
+        {/* might use later: */}
+        {/* <input
+          className="pageField"
+          type="number"
+          onChange={inputPagenumHandler}
+          value={pagenum}
+        ></input> */}
+        <button onClick={buttonRightHandler}>&rarr; next page</button>
+      </div>
     </div>
-    {/* Page navigation */}
-    <div className="pageNavigation">
-      <button>&larr; prev page</button>
-      <input className="pageField" type="number"></input>
-      <button>&rarr; next page</button>
-    </div>
-  </div>
-);
+  );
+};
