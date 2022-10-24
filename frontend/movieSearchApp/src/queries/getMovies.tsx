7290bf186cf9af93f6a32a88b437a162fb3313df
Mats Klevstad <matskl@stud.ntnu.no>
2022-10-24 14:39:10 +0200

    Update GET_MOVIES query to include Release Year

--- frontend/movieSearchApp/src/queries/getMovies.tsx
+++ frontend/movieSearchApp/src/queries/getMovies.tsx
@@ -3,8 +3,8 @@ import { gql } from "@apollo/client";
 export const GET_MOVIES = gql`
   query FindMovieByTitle($searchString: String, $offset: Int, $limit: Int) {
     findMovieByTitle(searchString: $searchString, offset: $offset, limit: $limit) {
-      Poster_Link,
       Series_Title,
+      Released_Year,
       IMDB_Rating
     }
   }
