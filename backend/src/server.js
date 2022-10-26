e165d0f71028a37f4139d0840af54e062f9d3f0a
Brage Hagemann Brataas <bragehb@stud.ntnu.no>
2022-10-26 16:45:41 +0200

    #10 Started working on addSearchQuery mutation.
    Co-authored-by: Mats Klevstad <matskl@stud.ntnu.no>

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,76 +6,67 @@ require("dotenv").config();
 //Sourcecode gotten from ""
 //Types for use in schema generation for Neo4J database, is fetched by neoSchema further down
 const typeDefs = gql`
-    type Movie {
-        Certificate: String
-        Director: String
-        Genre: String
-        Gross: String
-        IMDB_Rating: String
-        Meta_score: String
-        No_of_Votes: String
-        Overview: String
-        Poster_Link: String
-        Released_Year: String
-        Runtime: String
-        Series_Title: String!
-        Star1: String
-        Star2: String
-        Star3: String
-        Star4: String
-    }
+  type Movie {
+    Certificate: String
+    Director: String
+    Genre: String
+    Gross: String
+    IMDB_Rating: String
+    Meta_score: String
+    No_of_Votes: String
+    Overview: String
+    Poster_Link: String
+    Released_Year: String
+    Runtime: String
+    Series_Title: String!
+    Star1: String
+    Star2: String
+    Star3: String
+    Star4: String
+  }
 
-    type User {
-        name: String!
-        queries: [SearchQuery!]
-    }
+  type User {
+    email: String!
+    queries: [SearchQuery!]! @relationship(type: "SEARCHED_FOR", direction: OUT)
+  }
 
-    type SearchQuery {
-        user: User!
-        #Certificate: String
-        #Director: String
-        #Genre: String
-        #Gross: String
-        #IMDB_Rating: String
-        #Meta_score: String
-        #No_of_Votes: String
-        #Overview: String
-        #Poster_Link: String
-        #Released_Year: String
-        #Runtime: String
-        Series_Title: String!
-        #Star1: String
-        #Star2: String
-        #Star3: String
-        #Star4: String
-    }
-    type Query {
-        findMovieByTitle(searchString: String, offset: Int, limit: Int): [Movie] @cypher(
-            statement: """
-            CALL db.index.fulltext.queryNodes(
-                'titles', $searchString+'~') 
-            YIELD node RETURN node SKIP $offset LIMIT $limit
-            """
-  )
-}
+  type SearchQuery {
+    user: User! @relationship(type: "SEARCHED_FOR",direction: IN)
+    Series_Title: String!
+  }
+
+  type Query {
+    findMovieByTitle(searchString: String, offset: Int, limit: Int): [Movie]
+      @cypher(
+        statement: """
+        CALL db.index.fulltext.queryNodes(
+            'titles', $searchString+'~')
+        YIELD node RETURN node SKIP $offset LIMIT $limit
+        """
+      )
+  }
+
+  type Mutation {
+    addSearchQuery(user: UserCreateInput!, Series_Title: String!): SearchQuery
+  }
 `;
 
 //Driver for fetching data from Neo4j database, with generic user with read priviliges
 const driver = neo4j.driver(
-    process.env.NEO4J_URI,
-    neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
-  );
+  process.env.NEO4J_URI,
+  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
+);
 
 //Generates schema for Neo4J
 const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
 
 //Starts server
 neoSchema.getSchema().then((schema) => {
-    const server = new ApolloServer({
-        schema, 
-    });
-  
-    server.listen().then(({ url }) => {
-        console.log(`🚀 Server ready at ${url}`);
-    });
-  })
+  const server = new ApolloServer({
+    schema,
+  });
+
+  server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
+});
